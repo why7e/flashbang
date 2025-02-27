5db15818bd80e852ce97b2204bfd09c67e83e9dd
Gabriel Phua <gabrielphua@gmail.com>
2025-02-28 00:24:51 +1100

    Updated index to respect browser dark/light themeing. Yes I did that. Yes it was so I could host my own.

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
-        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
+        <h1>Und*ck 2</h1>
+        <p>DuckDuckGo's bang redirects are too slow and Und*ck does not respect browser themes. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://gphua.com/search?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -21,11 +21,7 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        Fork of <a href="https://unduck.link" target="_blank">Und*ck</a> by <a href="https://x.com/theo" target="_blank">theo</a>
       </footer>
     </div>
   `;
